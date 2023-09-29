06728e407124a50aefe9db1a7b9e697e96f8044f
highashell <220100@astanait.edu.kz>
2023-09-29 16:44:55 +0600

    adding singletone pattern

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import "strategy-as1/soldier_strategy"
 
 func main() {
-	basicSoldier := soldier_strategy.BasicSoldier{}
+	soldier := soldier_strategy.GetConfig()
+	singletonSoldier := soldier_strategy.GetConfig()
+	basicSoldier := soldier
+	BasicSoldier1 := singletonSoldier
 	bowSoldier := soldier_strategy.BowSoldier{Soldier: basicSoldier}
 	shieldSoldier := soldier_strategy.ShieldSoldier{Soldier: basicSoldier}
 	shieldBowSoldier := soldier_strategy.ShieldSoldier{
@@ -16,9 +19,11 @@ func main() {
 	soldier2 := soldier_strategy.SoldierBehavior{SB: bowSoldier}
 	soldier3 := soldier_strategy.SoldierBehavior{SB: shieldSoldier}
 	soldier4 := soldier_strategy.SoldierBehavior{SB: shieldBowSoldier}
+	soldier5 := soldier_strategy.SoldierBehavior{SB: BasicSoldier1}
 
 	soldier1.DisplayStats()
 	soldier2.DisplayStats()
 	soldier3.DisplayStats()
 	soldier4.DisplayStats()
+	soldier5.DisplayStats()
 }
--- soldier_strategy/BasicSoldier.go
+++ soldier_strategy/BasicSoldier.go
@@ -1,18 +1,38 @@
 package soldier_strategy
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-type BasicSoldier struct {
+var once sync.Once
+
+type basicSoldier struct {
 }
 
-func (b BasicSoldier) Info() {
+func (b basicSoldier) Info() {
 	fmt.Println("I'm a basic Soldier")
 }
 
-func (b BasicSoldier) Attack() int {
+func (b basicSoldier) Attack() int {
 	return 1
 }
 
-func (b BasicSoldier) HealthPoints() int {
+func (b basicSoldier) HealthPoints() int {
 	return 100
+}
+
+var singleInstance *basicSoldier
+
+func GetConfig() *basicSoldier {
+	if singleInstance == nil {
+		once.Do(
+			func() {
+				fmt.Println("No instances created, creating one")
+				singleInstance = &basicSoldier{}
+			})
+	} else {
+		fmt.Println("Instance was already created, don't ruin singleton")
+	}
+	return singleInstance
 }
